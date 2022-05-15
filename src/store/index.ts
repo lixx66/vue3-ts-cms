ca345709609d9f6cd19586723d9a3a17da95b882
xuan <lixx66@hotmail.com>
2022-05-15 22:20:32 +0800

    feat: project init

---
+++ src/store/index.ts
@@ -0,0 +1,9 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+  state: () => ({ name: 23333333333 }),
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {}
+});
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>LOGIN</h1>
+  </div>
+</template>
