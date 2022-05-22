c7636dc072dd1ed4e96c9d64d391cb88cfa714fb
xuan <lixx66@hotmail.com>
2022-05-22 16:21:51 +0800

    feat: init

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 const { defineConfig } = require('@vue/cli-service');
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers');
+
 module.exports = defineConfig({
   lintOnSave: false,
   transpileDependencies: true
 });
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      require('unplugin-vue-components/webpack')({
+        resolvers: [ElementPlusResolver()]
+      }),
+      require('unplugin-auto-import/webpack')({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  }
+};
